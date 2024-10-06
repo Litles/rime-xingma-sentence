166cf41fcf84a2e15b739f3a5d5551d60cf627f9
Litles <litlesme@gmail.com>
2024-10-06 22:34:51 +0800

    星陈·三码整句

--- schema_yustar_fluid.py
+++ schema_yustar_fluid.py
@@ -8,10 +8,34 @@
 import os
 import re
 from schema_yujoy_fluid import SchemaYujoyFluid
+from func_lib import (
+    load_word_freq,
+    load_char_code,
+    get_charset
+)
 
 class SchemaYustarFluid(SchemaYujoyFluid):
     def __init__(self, dir_in: str, fname_full: str, dir_out: str, sname: str, version: str):
-        super().__init__(dir_in, fname_full, dir_out, sname, version)
+        # 输入
+        print("初始化中...")
+        self.dir_in = dir_in
+        self.dict_char_codes = load_char_code(os.path.join(self.dir_in, fname_full), full_only=False)  # 单字全码码表(大概100,000字)
+        self.file_quick1 = os.path.join(self.dir_in,"quick_chars.txt")  # 指定一二简(字符)
+        self.file_quick2 = os.path.join(self.dir_in,"quick_others.txt")  # 指定二三简(其它)
+        self.list_char_code = []  # 将以包含 "char","code","freq" 三个键的 dict 为元素
+        self.set_qc_char = set()  # 记录所设的简字
+        # 子集和词频相关数据
+        self.dict_char_freq = load_word_freq("字词频/知频_字频.txt")  # 字频(词频对于曰常聊天优于"25亿")
+        self.dict_word_freq = load_word_freq("字词频/知频_字词频.txt")  # 知频_字词频.txt (里面的字符都在GBK汉字+[a-z|A-Z|\d]范围内, 但只包含7471个通规字)
+        self.set_base = get_charset("字符集/G标/GB18030汉字集_无兼容汉字.txt", "字符集/G标_通规/通规（8105字）.txt")
+        self.set_base.add("〇")  # 该字被收录到符号区，但应作为汉字使用，故加之
+        # 输出
+        self.dir_out = dir_out
+        self.sname = sname
+        self.version = version
+        if not os.path.exists(self.dir_out):
+            os.makedirs(self.dir_out)
+
         # 1.调整字根字全码为两码
         n_altered = 0
         for char, codes in self.dict_char_codes.items():
